f72d59f2517d52fe277bb3cf490fc61d73b04ee9
adriig <adriagarciapons3@gmail.com>
2022-05-08 23:10:35 +0200

    asd

---
+++ build/classes/habilidadesBase.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Habilidades = void 0;
+class Habilidades {
+    constructor(id, text) {
+        this.id = id,
+            this.text = text;
+    }
+}
+exports.Habilidades = Habilidades;
